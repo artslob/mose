87615969f702915db509840bdc7062b7f920af89
artslob <artslob@yandex.ru>
2019-05-06 11:58:39 +0300

    add local postgres db (in docker)

--- mose/settings.py
+++ mose/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'mose.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'wizuber',
+        'USER': 'user',
+        'PASSWORD': 'secret_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
