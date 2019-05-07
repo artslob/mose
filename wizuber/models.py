091b4800489af0067caa546ef78a7a1b18dd1bd4
artslob <artslob@yandex.ru>
2019-05-07 21:52:22 +0300

    add initial models for wishes, customer and wizard + migrations

--- wizuber/models.py
+++ wizuber/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Wizard(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30, null=True, blank=True)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30, null=True, blank=True)
+
+
+class Wishes(models.Model):
+    creator = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    description = models.TextField()
+    owner = models.ForeignKey(Wizard, on_delete=models.CASCADE, null=True, blank=True)
