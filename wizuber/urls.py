dda994197e8fb139f279adf5f948e8b02776de26
artslob <artslob@yandex.ru>
2019-05-07 22:51:34 +0300

    add all wizards and wizard detail views

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('wizards/', views.wizards, name='wizards'),
+    path('wizards/<int:wizard_id>/', views.wizard_detail, name='wizard_detail'),
 ]
--- wizuber/views.py
+++ wizuber/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from wizuber.models import Wizard
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the wizuber index.")
+
+
+def wizards(request):
+    all_wizards = Wizard.objects.all()
+    return render(request, 'wizuber/wizards.html', dict(wizards=all_wizards))
+
+
+def wizard_detail(request, wizard_id):
+    return render(request, 'wizuber/wizard_detail.html', dict(question=get_object_or_404(Wizard, pk=wizard_id)))
