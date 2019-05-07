2c7ad8ec3b665b75bdbf2a9166c615a546048ffb
artslob <artslob@yandex.ru>
2019-05-07 23:21:40 +0300

    change wizard view on generic list view

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'wizuber'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('wizards/', views.wizards, name='wizards'),
+    path('wizards/', views.WizardsView.as_view(), name='wizards'),
     path('wizards/<int:wizard_id>/', views.wizard_detail, name='wizard_detail'),
 ]
--- wizuber/views.py
+++ wizuber/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from wizuber.models import Wizard
 
@@ -7,9 +8,12 @@ def index(request):
     return render(request, 'wizuber/index.html')
 
 
-def wizards(request):
-    all_wizards = Wizard.objects.all()
-    return render(request, 'wizuber/wizards.html', dict(wizards=all_wizards))
+class WizardsView(generic.ListView):
+    template_name = 'wizuber/wizards.html'
+    context_object_name = 'wizards'
+
+    def get_queryset(self):
+        return Wizard.objects.all()
 
 
 def wizard_detail(request, wizard_id):
