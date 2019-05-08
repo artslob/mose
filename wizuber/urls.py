9dd6a47bc3c855d7d4e2ded46118bb7a8beebdf3
artslob <artslob@yandex.ru>
2019-05-08 21:19:24 +0300

    add customer signup

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'wizuber'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customer/signup/', views.CustomerSignUp.as_view(), name='customer_signup'),
     path('wizards/', views.WizardsView.as_view(), name='wizards'),
     path('wizards/<int:pk>/', views.WizardDetail.as_view(), name='wizard_detail'),
 ]
--- wizuber/views.py
+++ wizuber/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from wizuber.models import Wizard
@@ -6,6 +8,12 @@ from wizuber.models import Wizard
 
 def index(request):
     return render(request, 'wizuber/index.html')
+
+
+class CustomerSignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('wizuber:index')
+    template_name = 'wizuber/signup.html'
 
 
 class WizardsView(generic.ListView):
