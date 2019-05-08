0709d980222191230a3b61feeb7611e578f1e994
artslob <artslob@yandex.ru>
2019-05-08 21:49:22 +0300

    add login view

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -5,6 +6,7 @@ from . import views
 app_name = 'wizuber'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='wizuber/login.html'), name='login'),
     path('customer/signup/', views.CustomerSignUp.as_view(), name='customer_signup'),
     path('wizards/', views.WizardsView.as_view(), name='wizards'),
     path('wizards/<int:pk>/', views.WizardDetail.as_view(), name='wizard_detail'),
