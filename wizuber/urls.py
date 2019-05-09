57303f27b419d27e61eef5f49658e3f0e0ee24d9
artslob <artslob@yandex.ru>
2019-05-09 11:14:20 +0300

    change paths: accounts -> account

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -8,10 +8,10 @@ from . import views
 app_name = 'wizuber'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('accounts/', login_required(TemplateView.as_view(template_name='wizuber/account.html')), name='account'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='wizuber/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('accounts/signup/', views.CustomerSignUp.as_view(), name='signup'),
+    path('account/', login_required(TemplateView.as_view(template_name='wizuber/account.html')), name='account'),
+    path('account/login/', auth_views.LoginView.as_view(template_name='wizuber/login.html'), name='login'),
+    path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('account/signup/', views.CustomerSignUp.as_view(), name='signup'),
     path('wizards/', views.WizardsView.as_view(), name='wizards'),
     path('wizards/<int:pk>/', views.WizardDetail.as_view(), name='wizard_detail'),
 ]
