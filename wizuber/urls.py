434c2d758917f8d7bfb2307412bd76d30fd0b8c8
artslob <artslob@yandex.ru>
2019-11-03 13:28:26 +0300

    set redirect_authenticated_user to false

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -9,16 +9,15 @@ app_name = 'wizuber'
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('account/', login_required(TemplateView.as_view(template_name='wizuber/account.html')), name='account'),
-    path(
-        'account/login/',
-        auth_views.LoginView.as_view(template_name='wizuber/login.html', redirect_authenticated_user=True),
-        name='login'
-    ),
+    path('account/login/', auth_views.LoginView.as_view(template_name='wizuber/login.html'), name='login'),
     path('account/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('account/signup/', views.CustomerSignUp.as_view(), name='signup'),
+
     path('wizards/', views.WizardsView.as_view(), name='wizards'),
     path('wizards/<int:pk>/', views.WizardDetail.as_view(), name='wizard_detail'),
+
     path('wishes/', views.WishesList.as_view(), name='wishes'),
     path('wishes/new/', views.CreateWish.as_view(), name='create_wish'),
     path('wishes/<int:pk>/', views.WishDetail.as_view(), name='wish_detail'),
