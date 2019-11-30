fee36ba10fb102f03a0b526ca4064aa10bca3ce1
artslob <artslob@yandex.ru>
2019-11-30 13:58:21 +0300

    remove todo

--- wizuber/urls.py
+++ wizuber/urls.py
@@ -6,8 +6,6 @@ import wizuber.views as views
 
 app_name = 'wizuber'
 
-# TODO add deletion of artifacts
-
 urlpatterns = [
     path('', TemplateView.as_view(template_name='wizuber/index.html'), name='index'),
 
