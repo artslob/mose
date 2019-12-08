07ff99c710c2a44578ecdd4e3d9715189c108606
artslob <artslob@yandex.ru>
2019-12-08 11:12:17 +0300

    add helper method to get url for action

--- wizuber/tests/test_business_scenario.py
+++ wizuber/tests/test_business_scenario.py
@@ -46,6 +46,10 @@ class PrimaryBusinessScenario(TestCase):
 
     # TODO: check for artifact deletion
 
+    def get_url_for_action(self, action_name: str):
+        kwargs = dict(pk=self.wish.pk, action=action_name)
+        return reverse('wizuber:handle-wish-action', kwargs=kwargs)
+
     def wish_creating(self):
         self.assertEqual(Wish.objects.count(), 0)
         self.client.force_login(self.customer)
@@ -69,10 +73,9 @@ class PrimaryBusinessScenario(TestCase):
 
     def test_wish_deletion(self):
         self.wish_creating()
-        wish = self.check_created_wish_attributes()
+        self.check_created_wish_attributes()
 
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='delete'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('delete'))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Wish.objects.count(), 0)
 
@@ -83,8 +86,7 @@ class PrimaryBusinessScenario(TestCase):
         # pay for wish
         self.refresh()
         self.assertEqual(self.customer.balance, CUSTOMER_BALANCE)
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='pay'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('pay'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.in_status(WishStatus.ACTIVE))
@@ -95,8 +97,7 @@ class PrimaryBusinessScenario(TestCase):
         self.client.force_login(self.wizard)
         self.check_available_actions(wish, ['own'])
 
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='own'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('own'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.in_status(WishStatus.WORK))
@@ -108,8 +109,8 @@ class PrimaryBusinessScenario(TestCase):
         self.check_available_actions(wish, ['spirit-artifact', 'candle-artifact', 'pentacle-artifact', 'to-student'])
 
         # check candle artifact creation by wizard
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='candle-artifact'))
-        response = self.client.post(url, dict(material=CandleMaterial.TALLOW.name, size=SizeChoices.SMALL.name))
+        response = self.client.post(self.get_url_for_action('candle-artifact'),
+                                    dict(material=CandleMaterial.TALLOW.name, size=SizeChoices.SMALL.name))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertEqual(wish.candle_artifacts.count(), 1)
@@ -119,8 +120,7 @@ class PrimaryBusinessScenario(TestCase):
         self.assertEqual(candle.size, SizeChoices.SMALL.name)
 
         # check assign to student
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='to-student'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('to-student'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.owner == self.wizard)
@@ -130,8 +130,8 @@ class PrimaryBusinessScenario(TestCase):
         self.client.force_login(self.student)
         self.check_available_actions(wish, ['to-wizard', 'spirit-artifact', 'candle-artifact', 'pentacle-artifact'])
 
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='pentacle-artifact'))
-        response = self.client.post(url, dict(name='some test pentacle', size=SizeChoices.LARGE.name))
+        response = self.client.post(self.get_url_for_action('pentacle-artifact'),
+                                    dict(name='some test pentacle', size=SizeChoices.LARGE.name))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertEqual(wish.pentacle_artifacts.count(), 1)
@@ -142,8 +142,7 @@ class PrimaryBusinessScenario(TestCase):
         self.check_available_actions(wish, ['to-wizard', 'spirit-artifact', 'candle-artifact', 'pentacle-artifact'])
 
         # check spirit artifact creation by student
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='spirit-artifact'))
-        response = self.client.post(url, dict(spirit=self.spirit.pk))
+        response = self.client.post(self.get_url_for_action('spirit-artifact'), dict(spirit=self.spirit.pk))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.has_spirit_artifact())
@@ -152,8 +151,7 @@ class PrimaryBusinessScenario(TestCase):
         self.check_available_actions(wish, ['to-wizard', 'spirit-artifact', 'candle-artifact', 'pentacle-artifact'])
 
         # check assign to wizard
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='to-wizard'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('to-wizard'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.owner == self.wizard)
@@ -165,8 +163,7 @@ class PrimaryBusinessScenario(TestCase):
             'candle-artifact', 'spirit-artifact', 'pentacle-artifact', 'to-student', 'to-spirit'
         ])
 
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='to-spirit'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('to-spirit'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.in_status(WishStatus.ON_SPIRIT))
@@ -177,8 +174,7 @@ class PrimaryBusinessScenario(TestCase):
         self.client.force_login(self.spirit)
         self.check_available_actions(wish, ['spirit-to-wizard'])
 
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='spirit-to-wizard'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('spirit-to-wizard'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.in_status(WishStatus.READY))
@@ -189,8 +185,7 @@ class PrimaryBusinessScenario(TestCase):
         self.check_available_actions(wish, ['close'])
         self.assertEqual(self.wizard.balance, WIZARD_START_BALANCE)
 
-        url = reverse('wizuber:handle-wish-action', kwargs=dict(pk=wish.pk, action='close'))
-        response = self.client.post(url)
+        response = self.client.post(self.get_url_for_action('close'))
         self.assertEqual(response.status_code, 302)
         self.refresh()
         self.assertTrue(wish.in_status(WishStatus.CLOSED))
