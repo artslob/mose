f4ae73fa5317d64ca62e665e9457f0d4b2506bbb
artslob <artslob@yandex.ru>
2019-12-15 14:51:16 +0300

    add task: pay and delete

--- wizuber/tests/load_testing.py
+++ wizuber/tests/load_testing.py
@@ -1,20 +1,43 @@
-from locust import HttpLocust, TaskSet, constant, task
+from locust import HttpLocust, TaskSet, constant, task, seq_task, TaskSequence
 
 
 def wizuber_url(postfix: str = '') -> str:
     return f'/wizuber/{postfix}'
 
 
-def get_csrf_token(task_set: TaskSet, url: str):
-    response = task_set.client.get(url)
+def get_csrf_token(task_set: TaskSet, url: str, name=None):
+    """ https://stackoverflow.com/questions/51693930 """
+    response = task_set.client.get(url, name=name)
     csrf_token = response.cookies['csrftoken']
     return csrf_token, {
         'X-CSRFToken': csrf_token,
-        'Referer': task_set.parent.host + url
+        # 'Referer': task_set.parent.host + url
     }
 
 
+class CreateAndDeleteForWish(TaskSequence):
+    @seq_task(1)
+    def create_and_pay_wish(self):
+        create_wish_url = wizuber_url('wish/create/')
+        csrf_token, headers = get_csrf_token(self, create_wish_url)
+        data = dict(description='load testing wish', price=322)
+        response = self.client.post(create_wish_url, data, headers=headers)
+        self.created_wish_url = response.url
+
+    @seq_task(2)
+    def pay_for_wish(self):
+        csrf_token, headers = get_csrf_token(self, self.created_wish_url, name=wizuber_url('wish/[id]/'))
+        pay_url = self.created_wish_url + 'handle/delete'
+        self.client.post(pay_url, headers=headers, name=wizuber_url('wish/[id]/handle/delete'))
+
+    @seq_task(3)
+    def stop(self):
+        self.interrupt()
+
+
 class CustomerTask(TaskSet):
+    tasks = [CreateAndDeleteForWish]
+
     def on_start(self):
         login_url = wizuber_url('account/login/')
         csrf_token, headers = get_csrf_token(self, login_url)
@@ -55,13 +78,6 @@ class CustomerTask(TaskSet):
     @task
     def wish_list_closed(self):
         self.client.get(wizuber_url('wish/list/closed/'))
-
-    @task
-    def create_wish(self):
-        create_wish_url = wizuber_url('wish/create/')
-        csrf_token, headers = get_csrf_token(self, create_wish_url)
-        data = dict(description='load testing wish', price=322)
-        self.client.post(create_wish_url, data, headers=headers)
 
 
 class CustomerUser(HttpLocust):
